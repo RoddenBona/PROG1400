50cce73496e9cd3352516b50cb0d2709bc8382ce
RoddenBona <146233859+RoddenBona@users.noreply.github.com>
2024-02-01 10:56:31 -0400

    Added OOP files from the previous prog file. Tis is now the main OOP repo

---
+++ OOP_animal_Fruit_classes.py
@@ -0,0 +1,77 @@
+#This is to demonstrate inhieritance in OOP
+
+#Create the general animal class
+class Animal:
+    def __init__(self,name):
+        self.name = name
+    
+    def speak(self):
+        pass
+
+#Then at least 1-2 more classes for some animals
+class Dog(Animal):
+    def speak(self):
+        return(f"{self.name} says Woof")
+    
+class Cat(Animal):
+    def speak(self):
+        return(f"{self.name} says Meow")
+    
+
+#Create objects of a dog and cat
+dog = Dog("Kobe")
+cat = Cat("Finn")
+
+
+#Use methods
+print(dog.name)
+print(cat.name)
+print(F"{dog.speak()} and {cat.speak()}")
+
+#The Dog and Cat classes ingierit the name properties of the general Animal class
+
+
+
+
+#Polymorphism in python allows objects of different types to inhierit objects of a common type
+class Fruit:
+    def __init__(self,name,):
+        self.name = name
+
+    #Method to return "generic taste"
+    def taste(self):
+        return "Generic Taste"
+
+class Apple(Fruit):
+    def __init__ (self, name, color):
+    #Super is a built in function used usually in OOP
+    #Used specifically within classes that are part of an inhieritance hierarchy
+    #It provides a way to access and call methods from a superclass (parent class)
+    #in a subclass (child class)
+
+        super().__init__(name)
+        self.color = color
+
+    def taste(self):
+        generic_taste = super().taste() #Calling the superclass method
+        return f"{generic_taste} and Crispy"
+    
+class Orange(Fruit):
+    def __init__ (self, name, size):
+        super().__init__(name)
+        self.size = size
+
+    def taste(self):
+        generic_taste = super().taste() #Calling the superclass method
+        return f"{generic_taste} and Juicy"
+
+def describe_taste(Fruit):
+    print (f"{Fruit.name} tastes: {Fruit.taste()}")
+
+#make instances of Apples and Oranges
+apple = Apple("Honeydew", "Yellow")
+orange = Orange("Orange", "Large")
+
+#Demonstrate polymorphism by calling the describe taste function with different fruit objects
+describe_taste(apple)
+describe_taste(orange)
---
+++ W0461260_OOP_Basic_Class_Inhiertance.py
@@ -0,0 +1,67 @@
+#This file is just built off of the previous Basic class file. ust with Inhieritance in it
+
+#Creating a rectangle and define the attributes and methods of it to calculate it's area and perimeter
+
+#define the class. classes are templates for objects
+#n its's deffinition is the __init__ method which initializes it's attributes
+#the self attribute is a reference to the instacne to the class. Every class needs it
+#Length and width are attributes of the class
+#We'll be calculating the area and perameter of the rectangle using a calculate_area and calculate_perimeter 
+class Rectangle:
+    def __init__(self,length,width,height):
+        self.length = length
+        self.width = width
+        self.height = height
+
+    def calculate_area(self):
+        return self.length*self.width
+    
+    def calculate_volume(self):
+        return self.length*self.width*self.height
+
+    def calculate_perimeter(self):
+        return 2*(self.length + self.width)
+    
+#create an object using the provided class and it's attributes
+rect = Rectangle(length = 6, width = 4, height= 5)
+
+#access the object's attributes
+#We use dot (.) notation to access singular attribures of an object
+print(f"Length: {rect.length}")
+print(f"Width: {rect.width}")
+print(f"Height: {rect.height}")
+
+#print(type(rect))
+
+#Calling object methods
+area = rect.calculate_area()
+print(f"Area in 2D: {area}")
+
+perimeter = rect.calculate_perimeter()
+print(f"Perimeter: {perimeter}")
+
+volume = rect.calculate_volume()
+print(f"Volume: {volume}")
+
+
+#Create a new class called Square which will inhierot te attributes of Retangle
+class Sqaure(Rectangle):
+    def __init__(self, side_length):
+        #Call the constructor of the base class (Rectangle)
+        #But instead we are inserting our own extra thing. Where we make the lengh,width, and heigth equal to the one side length variable.
+        super().__init__(length = side_length, width=side_length, height=side_length)
+
+#Make te instnce of the square class
+#But now we only need tp fill one variable as side_length will just fill in
+#the 3 measuremnt variables that it inhierited with itself.
+cube = Sqaure(side_length=3)
+
+#we can use the same methods we made for the original Rectangle class since we inhierited them
+area_squared = cube.calculate_area()
+print(f"Square Area: {area_squared}") 
+
+volume_squared = cube.calculate_volume()
+print(f"Square Volume: {volume_squared}")
+
+perimeter_squared = cube.calculate_perimeter()
+print(f"Square Perimeter: {perimeter_squared}")
