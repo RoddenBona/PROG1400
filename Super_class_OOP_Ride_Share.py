69725009d7b3cc40574e06183f137df358b6ea28
RoddenBona <w0461260@campus.nscc.ca>
2024-02-15 12:34:51 -0400

    Code redone. Works fine so far

--- Super_class_OOP_Ride_Share.py
+++ Super_class_OOP_Ride_Share.py
@@ -1,46 +1,54 @@
+from datetime import datetime
 class User:
-    def __init__(self,first_name,last_name,address,phone,email):
+    def __init__(self,first_name,last_name,phone,email):
         self.first_name = first_name
         self.last_name = last_name
-        self.address = address
         self.phone = phone
         self.email = email
-    def __str__(self):
-        return  self.__class__.__name__
-    def make_drive(self,route):
-        print(f"{self.first_name} {self.last_name} is making a trip along the route of:{route}")
-
+    
+    def details(self):
+        return f"Name: {self.first_name} {self.last_name} Mobile: {self.phone} Email: {self.email}"
 class Driver(User):
-    def __init__(self, first_name, last_name, address, phone, email,v_make, v_model,v_color, v_type):
-        super().__init__(first_name, last_name, address, phone, email)
+    def __init__(self, first_name, last_name, phone, email,v_make, v_model,v_color, v_type):
+        super().__init__(first_name, last_name, phone, email)
         self.v_make = v_make
         self.v_model = v_model
         self.v_color = v_color
         self.v_type = v_type
 
-    def get_stops(self, route):
-        for stops in range(len(route)):
-            if stops [0] or stops[len(route)-1] == False:
-                return stops
-
-    def make_drive(self,route):
-        print(f"{self.first_name} {self.last_name} is driving a {self.v_color} {self.v_make} {self.v_model} {self.v_type} along the route\
-            leaving from {route[0]} and arriving at {route[len(route) -1]} and stopping at {route.get_stops()}") 
+    def details(self):
+        return f"{super().details()} Vehicle: {self.v_color} {self.v_make} {self.v_model} {self.v_type}"
 class Rider(User):
-    def __init__(self, first_name, last_name, address, phone, email):
-        super().__init__(first_name, last_name, address, phone, email)
-        
-    def make_drive(self, route):
-        print(f"{self.first_name} {self.last_name} will pick you up along the {route} route")
+    def __init__(self, first_name, last_name, phone, email):
+        super().__init__(first_name, last_name, phone, email)
+
+    def details(self):
+        return f"{super().details()}"
 class Route:
-    def __init__(self, stops):
-        self.stops = stops
-    def __str__(self):
-        pass
+    def __init__(self, origin, midway, destination):
+        self.origin = origin
+        self.midway = midway
+        self.destination = destination
+    def details(self):
+        return f"Origin: {self.origin} Midway: {self.midway} Destination: {self.destination}"
+class Drive(Route):
+    def __init__(self, origin, leave_time, midway, midway_time, destination, arrival_time):
+        super().__init__(origin, midway, destination)
+        self.leave_time = leave_time
+        self.midway_time = midway_time
+        self.arrival_time = arrival_time
+    def details(self):
+        return f"{super().details()} Leaving Time: {self.leave_time} Midway Stop Time: {self.midway_time} Arrival Time: {self.arrival_time}"
+    
+def main():
+    driver1 = Driver("Rodden", "Bona", "902-902-9029", "W0461260@campus.nscc.ca", "Ford", "AU Falcon", "Yellow", "SUV")
+    rider1 = Rider("Jim", "Caddick", "902-555-5555", "JCaddick@gmail.com")
+    trip1 = Drive("Antigonish",datetime(2024,2,15,8,0),"Tim Hortons",datetime(2024,2,15,8,30), "Strait Area Campus",datetime(2024,2,15,9,0))
 
-driver1 = Driver("Rodden", "Bona","36 Summit Dr" ,"902-300-5767","roddenbona2003@gmail.com", "Kia", "Soul", "Yellow", "SUV")
-rider1 = Rider("Will", "Williams","10 Existance St" ,"902-9029-029", "W.Williams@hotmail.com")
-route = ["Antigonish","Tim Hortons", "Strait Area Campus"]
+    travel1 = [driver1,rider1,trip1]
 
-driver1.make_drive(route)
-rider1.make_drive(route)
+    for i in travel1:
+        print(i.details())
+
+if __name__ == "__main__":
+    main()
