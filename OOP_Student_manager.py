17bac399b884f05c5fb8a15d2e3babd82477f976
RoddenBona <W0461260@campus.nscc.ca>
2024-03-06 14:45:03 -0400

    Not sure if it wokrs yet. Will try soon

--- OOP_Student_manager.py
+++ OOP_Student_manager.py
@@ -1,45 +1,81 @@
-
 students_list = []
 class Student:
     def __init__(self, student_id, name, age, grade):
-        self.student_id = 1
+        self.student_id = 0
         self.name = ""
         self.age = 0
         self.grade = 0
     
-    def display_info(self, name, age,):
-        self.__name = name
-
-
-        return f"Name: {self.name} Age: {self.age}, Grade: {self.grade}"
+    def display_info(self, name, age):
+        pass
     
-    def get_name(self):
-        return self.name
-
-    def get_age(self):
-        return self.age
-
-    def get_grade(self):
-        return self.grade
+    def update_info(self):
+        self.name = input("Enter new name: ")
+        self.age = input("Enter new age: ")
+        self.grade = input("Enter new grade")
+        print("Information has been updated")
 
 class GradeSudent(Student):
     def __init__(self, student_id, name, age, grade, thesis_topic):
         super().__init__(student_id, name, age, grade)
         self.thesis_topic = thesis_topic
     def display_info(self):
-        return f"{self.thesis_topic}"
-    
+        return f"Name: {self.name} Thesis: {self.thesis_topic}"
 
-def add_student():
-    new_name = input("What is the student's name?: ")
-    new_age = input("What is the student's age? ")
-    if new_age.isdigit():
-        new_age = int(new_age)
-    new_grade = input("What is the student's current grade? ")
-    if new_grade.isdigit():
-        new_grade = int(new_grade)
-    new_id = len(students_list) + 1
+class StudentManagementSystem(Student):
+    def __init__(self, student_id, name, age, grade):
+        super().__init__(student_id, name, age, grade)
 
-    def display_all_students():
+    def main_menu():
+        while True:
+            option = input("""Main menu
+                        Please Select an option below
+                        
+                        1) Add student
+                        2) All student information
+                        3) Search for a student
+                        4)Grade student
+                        """)
+            if option.isdigit():
+                option = int(option)
+            if option == 1:
+                StudentManagementSystem.add_student()
+            else:
+                if option == 2:
+                    StudentManagementSystem.display_all_students()
+                else:
+                    if option == 3:
+                        StudentManagementSystem.student_search()
+                    else:
+                        if option == 4:
+                            GradeSudent.display_info()
+                        else:
+                            print()
+
+    def add_student():
+        id = len(students_list) + 1
+        name = input("Enter new student name: ")
+        age = input("Enter new students age: ")
+        if age.isdigit():
+            age = int(age)
+        grade = input("Enter new student grade: ")
+        if grade.isdigit():
+            grade = int(grade)
+        new_student = Student(id, name, age, grade)
+
+    def display_all_students(self):
         for i in students_list:
-            pass
+            self.display_info()
+
+            #print(f"Student ID: {self.student_id} Name: {self.name} Age: {self.age} {self.grade}")
+
+    def student_search(self):
+        search = input("Enter Student ID:")
+        if self.student_id in students_list:
+            self.display_info
+
+
+
+example = Student(1, "Roland", 21, 85)
+
+students_list.append(example)
