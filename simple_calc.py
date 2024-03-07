97593ba0505ecce8ea403fe8fb2955e44019fea1
RoddenBona <w0461260@campus.nscc.ca>
2024-03-07 12:03:44 -0400

    need to import calc to test

--- simple_calc.py
+++ simple_calc.py
@@ -1,9 +1,20 @@
-#define the functions for basic math applications
+#define a class for advanced calculations
+#The @staticmethod decorator is used to define a method within a class
+# that does not access or modify the class or instance state
+#his meansthat the method does not have access to the instance (self)
+# or the class (cls) and behave like a regula function within the class
 
+# When you define a method within a class, by default it is an instance method,
+# meaning it operates on an instance of the class and has access to the instance
+# attributes via the self parameter. However, sometimes you may want to define a
+# method that does not require access to the instance or the class itself.
 class AdvancedCalc:
-    pass
+    @staticmethod
+    def exponentials(base, exponent):
+        return base ** exponent
 
 
+#define the functions for basic math applications
 def add(x,y):
     return x + y
 
@@ -19,6 +30,3 @@ def divide(x,y):
     else:
         return x / y
     
-def exponent(x,y):
-    return x ** y
-
